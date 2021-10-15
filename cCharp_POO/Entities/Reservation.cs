fcfe25b8bcae802f2b14b288a4c6928cfd5332c4
Werik Santos <werik.msti@gmail.com>
2021-10-15 19:50:46 -0300

    more codes

---
+++ cCharp_POO/Entities/Reservation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace cCharp_POO.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+            {
+                return "Error in reservation: Reservation dates for update must be future dates. ";
+            }
+
+            if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-out date must be after check-in date. ";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + "nigths";
+        }
+    }
+}
--- cCharp_POO/Program.cs
+++ cCharp_POO/Program.cs
@@ -10,188 +10,275 @@ namespace cCharp_POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number of POO problem: ");
+            Console.WriteLine("Enter a number of POO problem or 0 to out of system: ");
             Console.WriteLine("1 - Bank");
             Console.WriteLine("2 - Sale Product");
             Console.WriteLine("3 - Share Area");
             Console.WriteLine("4 - Tax Payer");
+            Console.WriteLine("5 - Try Except");
+            Console.WriteLine("6 - Host Reserve");
             int problem = int.Parse(Console.ReadLine());
 
-            switch (problem)
+            while (!(problem == 0))
             {
-                case 1:
-                    {
-                        Account acc1 = new Account(1101, "Alex", 500.0);
-                        Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
-
-                        acc1.Withdraw(10.0);
-                        acc2.Withdraw(10.0);
-
-                        Console.WriteLine(acc1.Balance);
-                        Console.WriteLine(acc2.Balance);
-                    }
-                    break;
-
-                case 2:
-                    {
-                        Console.WriteLine("Enter a number of products");
-                        int n = int.Parse(Console.ReadLine());
-
-                        List<Product> product = new List<Product>();
-
-                        for (int i = 1; i <= n; i++)
+                switch (problem)
+                {
+                    case 1:
                         {
-                            Console.WriteLine($"Product #{i} data:");
-                            Console.Write("Common, used or imported (c/u/i)?: ");
-                            char productType = char.Parse(Console.ReadLine());
+                            Account acc1 = new Account(1101, "Alex", 500.0);
+                            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
 
-                            Console.Write("Name: ");
-                            string productName = Console.ReadLine();
+                            acc1.Withdraw(10.0);
+                            acc2.Withdraw(10.0);
 
-                            Console.Write("Price: ");
-                            double productPrice = double.Parse(Console.ReadLine());
+                            Console.WriteLine(acc1.Balance);
+                            Console.WriteLine(acc2.Balance);
+                        }
+                        break;
 
+                    case 2:
+                        {
+                            Console.WriteLine("Enter a number of products");
+                            int n = int.Parse(Console.ReadLine());
 
-                            if (productType == 'c')
+                            List<Product> product = new List<Product>();
+
+                            for (int i = 1; i <= n; i++)
                             {
-                                product.Add(new Product(productName, productPrice));
+                                Console.WriteLine($"Product #{i} data:");
+                                Console.Write("Common, used or imported (c/u/i)?: ");
+                                char productType = char.Parse(Console.ReadLine());
+
+                                Console.Write("Name: ");
+                                string productName = Console.ReadLine();
+
+                                Console.Write("Price: ");
+                                double productPrice = double.Parse(Console.ReadLine());
+
+
+                                if (productType == 'c')
+                                {
+                                    product.Add(new Product(productName, productPrice));
+                                }
+                                else if (productType == 'i')
+                                {
+                                    Console.Write("Customs fee: ");
+                                    double customsFee = double.Parse(Console.ReadLine());
+
+                                    product.Add(new ImportedProduct(productName, productPrice, customsFee));
+                                }
+                                else
+                                {
+                                    Console.Write("Manufacture date (DD/MM/YYYY):");
+                                    DateTime productManufacture = DateTime.Parse(Console.ReadLine());
+                                    product.Add(new UsedProduct(productName, productPrice, productManufacture));
+                                }
+
                             }
-                            else if (productType == 'i')
+                            Console.WriteLine("PRICE TAGS:");
+                            foreach (Product prod in product)
                             {
-                                Console.Write("Customs fee: ");
-                                double customsFee = double.Parse(Console.ReadLine());
-
-                                product.Add(new ImportedProduct(productName, productPrice, customsFee));
-                            }
-                            else
-                            {
-                                Console.Write("Manufacture date (DD/MM/YYYY):");
-                                DateTime productManufacture = DateTime.Parse(Console.ReadLine());
-                                product.Add(new UsedProduct(productName, productPrice, productManufacture));
+                                Console.WriteLine(prod.PriceTag());
                             }
 
                         }
-                        Console.WriteLine("PRICE TAGS:");
-                        foreach (Product prod in product)
+                        break;
+
+                    case 3:
                         {
-                            Console.WriteLine(prod.PriceTag());
-                        }
+                            List<Shape> list = new List<Shape>();
 
-                    }
-                    break;
+                            Console.WriteLine("Enter the number of shapes: ");
+                            int n = int.Parse(Console.ReadLine());
 
-                case 3:
-                    {
-                        List<Shape> list = new List<Shape>();
-
-                        Console.WriteLine("Enter the number of shapes: ");
-                        int n = int.Parse(Console.ReadLine());
-
-                        for (int i = 1; i <= n; i++)
-                        {
-                            Console.WriteLine($"Shape #{i} data: ");
-
-                            Console.Write("Rectangle or Circle (r/c)? ");
-                            char type = char.Parse(Console.ReadLine());
-
-                            Console.Write("Color (Black/Blue/Red): ");
-                            Color color = Enum.Parse<Color>(Console.ReadLine());
-
-                            if (type == 'r')
+                            for (int i = 1; i <= n; i++)
                             {
-                                Console.Write("Widht: ");
-                                double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                                Console.WriteLine($"Shape #{i} data: ");
 
-                                Console.Write("Height: ");
-                                double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                                Console.Write("Rectangle or Circle (r/c)? ");
+                                char type = char.Parse(Console.ReadLine());
 
-                                list.Add(new Rectangle(width, height, color));
-                            }
-                            else
-                            {
-                                Console.Write("Radius: ");
-                                double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                                Console.Write("Color (Black/Blue/Red): ");
+                                Color color = Enum.Parse<Color>(Console.ReadLine());
 
-                                list.Add(new Circle(radius, color));
+                                if (type == 'r')
+                                {
+                                    Console.Write("Widht: ");
+                                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                            }
+                                    Console.Write("Height: ");
+                                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                        }
+                                    list.Add(new Rectangle(width, height, color));
+                                }
+                                else
+                                {
+                                    Console.Write("Radius: ");
+                                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                        Console.WriteLine();
-                        Console.WriteLine("SHAPE AREAS: ");
+                                    list.Add(new Circle(radius, color));
 
-                        foreach (Shape shape in list)
-                        {
-                            Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
-                        }
+                                }
 
-                    }
-                    break;
-
-                case 4:
-                    {
-                        Console.WriteLine("Enter the number of tax payers: ");
-                        int n = int.Parse(Console.ReadLine());
-
-                        List<TaxPayer> list = new List<TaxPayer>();
-
-                        for (int i = 1; i <= n; i++)
-                        {
-                            Console.Write($"Tax payer #{i}: ");
-
-                            Console.Write("Individual company(i/c)?");
-                            char type = char.Parse(Console.ReadLine());
-
-                            Console.Write("Name: ");
-                            string name = Console.ReadLine();
-
-                            Console.Write("Anual income:");
-                            double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                            if (type == 'i')
-                            {
-                                Console.Write("Health expenditures:");
-                                double healthExpenditures = double.Parse(Console.ReadLine());
-                                list.Add(new Individual(name, anualIncome, healthExpenditures));
-                            }
-                            else
-                            {
-                                Console.Write("Number of employees:");
-                                int numberEmployees = int.Parse(Console.ReadLine());
-                                list.Add(new Company(name, anualIncome, numberEmployees));
                             }
 
                             Console.WriteLine();
+                            Console.WriteLine("SHAPE AREAS: ");
+
+                            foreach (Shape shape in list)
+                            {
+                                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+                            }
+
                         }
+                        break;
 
-                        Console.WriteLine();
-                        Console.WriteLine("TAX PAID:");
-
-                        foreach (TaxPayer txtpayer in list)
+                    case 4:
                         {
-                            Console.Write(txtpayer.Name + " $ ");
-                            Console.WriteLine(txtpayer.Tax().ToString("F2", CultureInfo.InvariantCulture));
+                            Console.WriteLine("Enter the number of tax payers: ");
+                            int n = int.Parse(Console.ReadLine());
+
+                            List<TaxPayer> list = new List<TaxPayer>();
+
+                            for (int i = 1; i <= n; i++)
+                            {
+                                Console.Write($"Tax payer #{i}: ");
+
+                                Console.Write("Individual company(i/c)?");
+                                char type = char.Parse(Console.ReadLine());
+
+                                Console.Write("Name: ");
+                                string name = Console.ReadLine();
+
+                                Console.Write("Anual income:");
+                                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                                if (type == 'i')
+                                {
+                                    Console.Write("Health expenditures:");
+                                    double healthExpenditures = double.Parse(Console.ReadLine());
+                                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                                }
+                                else
+                                {
+                                    Console.Write("Number of employees:");
+                                    int numberEmployees = int.Parse(Console.ReadLine());
+                                    list.Add(new Company(name, anualIncome, numberEmployees));
+                                }
+
+                                Console.WriteLine();
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine("TAX PAID:");
+
+                            foreach (TaxPayer txtpayer in list)
+                            {
+                                Console.Write(txtpayer.Name + " $ ");
+                                Console.WriteLine(txtpayer.Tax().ToString("F2", CultureInfo.InvariantCulture));
+                            }
+
+                            double totalTax = 0;
+
+                            foreach (TaxPayer txtpayer in list)
+                            {
+                                totalTax += txtpayer.Tax();
+                            }
+
+                            Console.WriteLine();
+                            Console.Write("TOTAL TAXES: $ " + totalTax.ToString("F2", CultureInfo.InvariantCulture));
+
                         }
+                        break;
 
-                        double totalTax = 0;
-
-                        foreach (TaxPayer txtpayer in list)
+                    case 5:
                         {
-                            totalTax += txtpayer.Tax();
+                            try
+                            {
+                                Console.WriteLine("Informe um número: ");
+                                int n1 = int.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Informe outro número: ");
+                                int n2 = int.Parse(Console.ReadLine());
+
+
+                                int result = n1 / n2;
+                                Console.Write("O resultado é: ");
+                                Console.WriteLine(result);
+
+                            }
+                            catch (DivideByZeroException)
+                            {
+                                Console.WriteLine("Divisão por zero não é permitida!");
+                            }
+                            catch (FormatException e)
+                            {
+                                Console.WriteLine("Format error! " + e.Message);
+                            }
+                            finally
+                            {
+                                Console.WriteLine("Finalizado");
+                                Console.WriteLine();
+                            }
+
                         }
+                        break;
 
-                        Console.WriteLine();
-                        Console.Write("TOTAL TAXES: $ " + totalTax.ToString("F2", CultureInfo.InvariantCulture));
+                    case 6:
+                        {
+                            // SOLUÇÃO RUIM :
 
-                    }
-                    break;
+                            Console.WriteLine("Room number: ");
+                            int roomNumber = int.Parse(Console.ReadLine());
 
-            };
+                            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                            if (checkOut <= checkIn)
+                            {
+                                Console.WriteLine("Error in reservation: Check-out date must be after check-in date. ");
+                            }
+                            else
+                            {
+                                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                                Console.WriteLine("Reservation: " + reservation);
+
+                                Console.WriteLine();
+                                Console.WriteLine("Enter data to update the reservation: ");
+
+
+                                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                                checkIn = DateTime.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                                checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                                if(error != null)
+                                {
+                                    Console.WriteLine("Error in reservation: " + error);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Reservation: " + reservation);
+                                }
+                            }
+                        }
+                        break;
+
+                }
+
+            }
 
         }
 
     }
-
 }
+
 
